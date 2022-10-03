c65fa1fd5a0146fd358aeb804609f37a0b9d43d5
Hamid Nazari <nhamid.info@gmail.com>
2022-10-03 03:38:45 +0200

    fixed the security issues

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+import React, { Component } from "react";
+import "./App.css";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       monsters: [],
-      searchfield: ''
-    }
+      searchfield: "",
+    };
   }
-  
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => { return response.json() })
-    .then(users => { this.setState({monsters: users}) })
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ monsters: users });
+      });
   }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
-
-  }
+  };
   render() {
-    const { monsters, searchfield} = this.state;
-    const filteredMonsters = monsters.filter(monster => {
+    const { monsters, searchfield } = this.state;
+    const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !monsters.length ? <h2>Loading...</h2> :
-      (
-        <div className="App">
-          <h1>MonstersFreind</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList monsters={filteredMonsters} />
-          </Scroll>
-        </div>
-      )
+    });
+    return !monsters.length ? (
+      <h2>Loading...</h2>
+    ) : (
+      <div className="App">
+        <h1>MonstersFreind</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList monsters={filteredMonsters} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
